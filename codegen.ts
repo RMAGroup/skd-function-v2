ff767237521811fd56ae630167f9e7927665a025
Paul Krisadaolarn <pauly.k@gmail.com>
2024-05-02 17:40:41 +0700

    fix: Update documents path in codegen.ts
    to Common/graphql/**/*.ts

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 export const config: CodegenConfig = {
     overwrite: true,
     schema: 'http://0.0.0.0:5100/graphql',
-    documents: "shared/graphql/**/*.ts",
+    documents: "Common/graphql/**/*.ts",
     generates: {
         'shared/graphql/generated/graphql.ts': {
             plugins: [
